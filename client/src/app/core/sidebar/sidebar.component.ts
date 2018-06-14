1382a576155f7943dcc11d45ebf01c57d04bf9d2
wgf4242 <wgf4242@gmail.com>
2018-06-15 06:47:51 +0800

    syncx

--- client/src/app/core/sidebar/sidebar.component.ts
+++ client/src/app/core/sidebar/sidebar.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  navItems = [
+    { span: '巡视检修记录',   icon: 'home',         link: '/line/tour' },
+    { span: '设备管理',       icon: 'devices',      link: '/line/facility' },
+    { span: '投产验收',       icon: 'home',         link: '/line/production' },
+    { span: '线路信息',       icon: 'settings',     link: '/line/info' },
+    { span: '检修管理',       icon: 'home',         link: '/line/repair' },
+    { span: '变压器管理',     icon: 'home',         link: '/line/transformer' },
+    { span: '跳闸统计',       icon: 'bar_chart',    link: '' },
+  ]
   constructor() { }
 
   ngOnInit() {
